f887266ad49b84962d076e869a350b0302ef3c06
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-05-10 22:25:50 +0900

    Card #13

---
+++ lib/13_card/card.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MyCard extends StatelessWidget {
+  const MyCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        elevation: 20,
+        color: Colors.orangeAccent,
+        child: Padding(
+          padding: const EdgeInsets.all(
+            15.0,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const SizedBox(
+                height: 8,
+              ),
+              const Text('This is a Flutter card'),
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Press',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
